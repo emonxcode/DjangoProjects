efb89bf0ccee1688a46442f03dedcbc9bd4978bf
ShahriarEmon3215 <shahriar3215emon@gmail.com>
2023-12-14 01:25:08 +0600

    end url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('post', views.post, name="post_list"),
     path('post/<int:id>', views.getPostById, name="post_details"),
+    path('post/update/<int:id>', views.postOperation, name="update_post"),
+    path('post/delete/<int:id>', views.postOperation, name="delete_post"),
 ]
